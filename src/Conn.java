0cb38cc13414b574b05368b7169f508fbd142fdb
Atom Alexander M. Nava <atommunoznava@gmail.com>
2024-08-09 16:44:31 -0600

    Update Conn.java
    Modificar el DriveManager.getConnection por:
    c = DriverManager.getConnection("jdbc:mysql://localhost:3306/bankmanagementsystem", "root", "12345678");

--- src/Conn.java
+++ src/Conn.java
@@ -8,11 +8,9 @@ public class Conn {
     Statement s;
     public Conn() {
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            c = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/bankmanagementsystem", "root", "");
+            c = DriverManager.getConnection("jdbc:mysql://localhost:3306/bankmanagementsystem", "root", "12345678");
             s = c.createStatement();
         } catch(Exception e) {
-            System.out.println("Error al conectar con la base de datos: " + e.getMessage());
             e.printStackTrace();
         }
     }
