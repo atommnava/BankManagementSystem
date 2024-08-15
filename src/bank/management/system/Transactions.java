eddc83cd45e576781b93e9dd83cf8a59d05f6d6b
mhnb <atommunoznava@gmail.com>
2024-08-15 13:26:36 -0600

    Creando clase de Consulta para balance

--- src/bank/management/system/Transactions.java
+++ src/bank/management/system/Transactions.java
@@ -83,6 +83,9 @@ public class Transactions extends JFrame implements ActionListener {
         } else if (e.getSource() == pinChange){
             setVisible(false);
             new PinChange(pinNumber).setVisible(true);
+        } else if (e.getSource() == balanceenQuiry){
+            setVisible(false);
+            new BalanceEnquiry(pinNumber).setVisible(true);
         }
     }
     public static void main(String[] args) {
