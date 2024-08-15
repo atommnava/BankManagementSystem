cbaaf1a28176934dad90c0b5fd9232fc33356f26
mhnb <atommunoznava@gmail.com>
2024-08-15 13:24:59 -0600

    Add files via upload

---
+++ src/bank/management/system/BalanceEnquiry.java
@@ -0,0 +1,61 @@
+package bank.management.system;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.ResultSet;
+
+public class BalanceEnquiry extends JFrame implements ActionListener {
+    JButton back;
+    String pinNumber;
+    BalanceEnquiry(String pinNumber){
+        this.pinNumber = pinNumber;
+        setLayout(null);
+
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/atm.jpg"));
+        Image i2 = i1.getImage().getScaledInstance(900,900, Image.SCALE_DEFAULT);
+        ImageIcon i3 = new ImageIcon(i2);
+        JLabel image = new JLabel(i3);
+        image.setBounds(0, 0, 900, 900);
+        add(image);
+
+        back = new JButton("Back");
+        back.setBounds(355, 520, 150, 30);
+        back.addActionListener(this);
+        image.add(back);
+
+        Conn c = new Conn();
+        int balance = 0;
+        try {
+            ResultSet rs = c.s.executeQuery("select * from bank where pin = '" + pinNumber + "'");
+            while (rs.next()) {
+                if (rs.getString("type").equals("Deposit")) {
+                    balance += Integer.parseInt(rs.getString("amount"));
+                } else {
+                    balance -= Integer.parseInt(rs.getString("amount"));
+                }
+            }
+        } catch (Exception ex) {
+                System.out.println(ex);
+            }
+        JLabel text = new JLabel("Your Current Account Balance is: Ds " + balance);
+        text.setForeground(Color.WHITE);
+        text.setBounds(170,300, 400, 30);
+        image.add(text);
+
+        setSize(900,900);
+        setLocation(300,0);
+        setVisible(true);
+
+    }
+    public static void main(String[] args) {
+        new BalanceEnquiry("");
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        setVisible(false);
+        new Transactions(pinNumber).setVisible(true);
+    }
+}
