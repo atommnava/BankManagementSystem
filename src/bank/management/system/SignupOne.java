aa3b6166a226b55d527f3f1a9ef638305a970a6c
Atom Alexander M. Nava <atommunoznava@gmail.com>
2024-08-16 20:54:16 -0600

    Documentación clase 'SignupOne'

--- src/bank/management/system/SignupOne.java
+++ src/bank/management/system/SignupOne.java
@@ -6,50 +6,68 @@ import java.util.Random;
 import com.toedter.calendar.JDateChooser;
 import java.awt.event.*;
 
+/*
+ * @brief La clase 'SignupOne' implementa en menor proporción la parte gráfica y se enfoca más en la
+ *        entrada de datos personales. Los datos quedan registrados en la base datos para que se pueda
+ *        acceder a ellos posteriormente. 
+ *        
+ *        Nota:
+ *        En el README especifico que para la entrada de datos para la fecha de nacimiento integro la librería
+ *        'jcalendar.jar', con el fin de ahorrar timepo y recursos.
+ * @author Atom Alexander M. Nava
+ * @date 16/08/24
+ */
+
 public class SignupOne extends JFrame implements ActionListener {
-    long random;
+
+    //Variables globales
     JTextField nameTextField, fnameTextField, dobTextField, emailTextField,
             addressTextField, cityTextField, stateTextField, pinCTextField;
     JButton next;
     JRadioButton male, female, other, married, unmarried;
     JDateChooser dateChooser;
+
+    //Código para generar el número del formulario aleatorio
+    Random ran = new Random();
+    long random = Math.abs((ran.nextLong() % 9000L) + 1000L);
+    String first = "" + Math.abs(random);
+
     SignupOne()
     {
         setLayout(null);
-        Random ran = new Random();
-        long random = Math.abs((ran.nextLong() % 9000L) + 1000L);
+        
         JLabel formNo = new JLabel("APPLICATION FORM NO. " + random);
-        formNo.setFont(new Font("Serif", Font.BOLD, 38));
+        formNo.setFont(new Font("Raleway", Font.BOLD, 38));
         formNo.setBounds(140, 20, 600, 40);
         add(formNo);
 
         JLabel personalDetails = new JLabel("Page 1: Personal Details");
-        personalDetails.setFont(new Font("Serif", Font.BOLD, 22));
+        personalDetails.setFont(new Font("Raleway", Font.BOLD, 22));
         personalDetails.setBounds(290, 80, 400, 30);
         add(personalDetails);
 
         JLabel name = new JLabel("Name:");
-        name.setFont(new Font("Serif", Font.BOLD, 20));
+        name.setFont(new Font("Raleway", Font.BOLD, 20));
         name.setBounds(100, 140, 100, 30);
         add(name);
 
         nameTextField = new JTextField();
-        nameTextField.setFont(new Font("Serif", Font.BOLD, 14));
+        nameTextField.setFont(new Font("Raleway", Font.BOLD, 14));
         nameTextField.setBounds(300, 140, 400, 30);
         add(nameTextField);
 
         JLabel fName = new JLabel("Father´s Name:");
-        fName.setFont(new Font("Serif", Font.BOLD, 20));
+        fName.setFont(new Font("Raleway", Font.BOLD, 20));
         fName.setBounds(100, 190, 200, 30);
         add(fName);
 
         fnameTextField = new JTextField();
-        fnameTextField.setFont(new Font("Serif", Font.BOLD, 14));
+        fnameTextField.setFont(new Font("Raleway", Font.BOLD, 14));
         fnameTextField.setBounds(300, 190, 400, 30);
         add(fnameTextField);
 
         JLabel dob = new JLabel("Date of Birth:");
-        dob.setFont(new Font("Serif", Font.BOLD, 20));
+        dob.setFont(new Font("Raleway", Font.BOLD, 20));
         dob.setBounds(100, 240, 200, 30);
         add(dob);
 
@@ -59,7 +77,7 @@ public class SignupOne extends JFrame implements ActionListener {
         add(dateChooser);
 
         JLabel gender = new JLabel("Gender:");
-        gender.setFont(new Font("Serif", Font.BOLD, 20));
+        gender.setFont(new Font("Raleway", Font.BOLD, 20));
         gender.setBounds(100, 290, 200, 30);
         add(gender);
 
@@ -78,17 +96,17 @@ public class SignupOne extends JFrame implements ActionListener {
         genderGroup.add(female);
 
         JLabel email = new JLabel("Email Address:");
-        email.setFont(new Font("Serif", Font.BOLD, 20));
+        email.setFont(new Font("Raleway", Font.BOLD, 20));
         email.setBounds(100, 340, 200, 30);
         add(email);
 
         emailTextField = new JTextField();
-        emailTextField.setFont(new Font("Serif", Font.BOLD, 14));
+        emailTextField.setFont(new Font("Raleway", Font.BOLD, 14));
         emailTextField.setBounds(300, 340, 400, 30);
         add(emailTextField);
 
         JLabel maritalS = new JLabel("Marital Status:");
-        maritalS.setFont(new Font("Serif", Font.BOLD, 20));
+        maritalS.setFont(new Font("Raleway", Font.BOLD, 20));
         maritalS.setBounds(100, 390, 200, 30);
         add(maritalS);
 
@@ -113,49 +131,49 @@ public class SignupOne extends JFrame implements ActionListener {
         maritalGroup.add(other);
 
         JLabel address = new JLabel("Address:");
-        address.setFont(new Font("Serif", Font.BOLD, 20));
+        address.setFont(new Font("Raleway", Font.BOLD, 20));
         address.setBounds(100, 440, 200, 30);
         add(address);
 
         addressTextField = new JTextField();
-        addressTextField.setFont(new Font("Serif", Font.BOLD, 14));
+        addressTextField.setFont(new Font("Raleway", Font.BOLD, 14));
         addressTextField.setBounds(300, 440, 400, 30);
         add(addressTextField);
 
         JLabel city = new JLabel("City:");
-        city.setFont(new Font("Serif", Font.BOLD, 20));
+        city.setFont(new Font("Raleway", Font.BOLD, 20));
         city.setBounds(100, 490, 200, 30);
         add(city);
 
         cityTextField = new JTextField();
-        cityTextField.setFont(new Font("Serif", Font.BOLD, 14));
+        cityTextField.setFont(new Font("Raleway", Font.BOLD, 14));
         cityTextField.setBounds(300, 490, 400, 30);
         add(cityTextField);
 
         JLabel state = new JLabel("State:");
-        state.setFont(new Font("Serif", Font.BOLD, 20));
+        state.setFont(new Font("Raleway", Font.BOLD, 20));
         state.setBounds(100, 540, 200, 30);
         add(state);
 
         stateTextField = new JTextField();
-        stateTextField.setFont(new Font("Serif", Font.BOLD, 14));
+        stateTextField.setFont(new Font("Raleway", Font.BOLD, 14));
         stateTextField.setBounds(300, 540, 400, 30);
         add(stateTextField);
 
         JLabel pinC = new JLabel("PIN Code:");
-        pinC.setFont(new Font("Serif", Font.BOLD, 20));
+        pinC.setFont(new Font("Raleway", Font.BOLD, 20));
         pinC.setBounds(100, 590, 200, 30);
         add(pinC);
 
         pinCTextField = new JTextField();
-        pinCTextField.setFont(new Font("Serif", Font.BOLD, 14));
+        pinCTextField.setFont(new Font("Raleway", Font.BOLD, 14));
         pinCTextField.setBounds(300, 590, 400, 30);
         add(pinCTextField);
 
         next = new JButton("Next");
         next.setBackground(Color.BLACK);
         next.setForeground(Color.BLACK);
-        next.setFont(new Font("Serif", Font.BOLD, 14));
+        next.setFont(new Font("Raleway", Font.BOLD, 14));
         next.setBounds(620, 660, 80, 30);
         next.addActionListener(this);
         add(next);
@@ -166,7 +184,14 @@ public class SignupOne extends JFrame implements ActionListener {
         setVisible(true);
 
     }
-
+    /*
+     * @brief Implementación de la interfaz 'ActionListener' para responder a los eventos del usuario.
+     *        Obtención de los datos para la primera página. Utilizando las funciones .getText() tanto getDateEditor()
+     *        es como podremos almacenar temporalmente losd datos registrados en la base de datos de la página 1.
+     * @param e. Parámetro 'e' sin usos.
+     * @author Atom Alexander M. Nava
+     * @date 16/08/24
+     */
     public void actionPerformed(ActionEvent e)
     {
         String formNo = "" + random;
@@ -195,6 +220,7 @@ public class SignupOne extends JFrame implements ActionListener {
         String state = stateTextField.getText();
         String pinC = pinCTextField.getText();
 
+        // Enlace con la base de datos y un query para insertar los datos de la página 1
         try {
             if (name.equals("")) {
                 JOptionPane.showMessageDialog(null, "Please enter your name");
@@ -212,7 +238,12 @@ public class SignupOne extends JFrame implements ActionListener {
         }
     }
 
-
+    /*
+     * @brief Método principal para desplegar la página 1, con su propia ventana independiente
+     *        a las otras clases para que sea más dinámico con el usuario.
+     * @author Atom Alexander M. Nava
+     * @date 16/08/24
+     */
     public static void main(String[] args) {
         new SignupOne();
     }
