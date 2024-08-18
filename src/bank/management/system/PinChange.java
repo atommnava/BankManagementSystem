76553d0230a312e7b4cd296cc00f76a8da3eb1d1
Atom Alexander M. Nava <atommunoznava@gmail.com>
2024-08-18 09:11:37 -0600

    Documentación FINAL Pin change
    Documentación final, clase 'PinChange'

--- src/bank/management/system/PinChange.java
+++ src/bank/management/system/PinChange.java
@@ -5,10 +5,29 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+/*
+ * @brief Clase 'PinChange' ofrece la opción de cambiar el PIN de la tarjeta del usuario. La clase integra la parte gráfica
+ *        la del cajero, proporcinando una estilo más realista. El usuario visualizara dos campos en el que se le solicitara
+ *        que ingrese el nuevo PIN reemplazara el anterior PIN. A continuación, se le solicita al cliente que ingrese nuevamente
+ *        el nuevo PIN, esta funcionalidad sa ha integrado con el tiempo en todos los cajeros ATM.
+ *        
+ *        La aplicación valida que se encuentre rellenado el campo donde el cliente ingresa el nuevo PIN, así como tambien que
+ *        el campo donde reescribes el PIN valida que no este vacío y que ambos campos coincidan.
+ * @author Atom Alexander M. Nava
+ * @date 18/08/24
+ */
 public class PinChange extends JFrame implements ActionListener {
+    // Variables globales
     JPasswordField pin, rePin;
     JButton change, back;
     String pinNumber;
+    /*
+     * @brief Constructor 'PinChange' extiende la parte gráfica para el desarrollo de la ventana con los campos requeridos
+     *        para el cambio de PIN. Estos son de tipo 'JPasswordField', un cambio relevante y que añade un atisbo de privacidad
+     *        y seguridad para los clientes.
+     * @author Atom Alexander M. Nava
+     * @date 18/08/24
+     */
     PinChange(String pinNumber){
         this.pinNumber = pinNumber;
         setLayout(null);
@@ -63,6 +82,16 @@ public class PinChange extends JFrame implements ActionListener {
         setUndecorated(true);
         setVisible(true);
     }
+    /*
+     * @brief Implementación de la interfaz 'ActionListener' para llevar a cabo el cambio de PIN de la tarjeta.
+     *        A fin de cambiar el PIN de la tarjeta el usuario debe haber verificado que haya lenado ambos campos 
+     *        y que el PIN coincida, en este momento, el cliente debe pulsar el boton 'Change'. El PIN quedara registrado
+     *        en la tarjeta y modificado sin corromper la integridad de la cuenta del cliente.
+     * @param e. Parámetro 'e' trabaja con e botón 'Change', cuando este es pulsado sabemos que es cuando el cliente 
+     *        desea realizar la orden para el cambio de PIN.
+     * @author Atom Alexander M. Nava
+     * @date 18/08/24
+     */
     @Override
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == change) {
@@ -85,7 +114,14 @@ public class PinChange extends JFrame implements ActionListener {
                     return;
                 }
 
+                // Conexión con la base de datos MySQL
+    
                 Conn conn = new Conn();
+
+                // Sobreescribe los datos del PIN de la tarjeta en particular sin corromper la integridad de la cuenta.
+                // Además, debe actualizar el PIN en la base de datos, especificamente en las mesas 'bank', 'login' y 'signupthree' ,
+                // en estas se encuentra el valor del PIN de cada tarjeta.
+
                 String query1 = "update bank set pin = '" + rPin + "' where pin = '" + pinNumber + "'";
                 String query2 = "update login set pin = '" + rPin + "' where pin = '" + pinNumber + "'";
                 String query3 = "update signupthree set pin = '" + rPin + "' where pin = '" + pinNumber + "'";
@@ -107,6 +143,12 @@ public class PinChange extends JFrame implements ActionListener {
         }
     }
 
+    /*
+     * @brief Método principal en donde se ejecutara la funcionalidad de la clase 'PinChange'. Para llevar a cabo el proceso
+     *        para el cambio de PIN, necesitamos la ayuda del cliente para que ingrese el nuevo PIN que reemplazara al anterior.
+     * @author Atom Alexander M. Nava
+     * @date 18/08/24
+     */
     public static void main(String[] args) {
         new PinChange("").setVisible(true);
     }
